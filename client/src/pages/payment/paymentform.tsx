d06f959c47d6336e59ffb8c67c1922d83705b1a3
Abdulmajid Awol <maajidawol@gmail.com>
2024-09-09 22:34:08 +0300

    tel

--- client/src/pages/payment/paymentform.tsx
+++ client/src/pages/payment/paymentform.tsx
@@ -21,7 +21,7 @@ const PaymentForm = () => {
 
   const handlePayment = async () => {
     try {
-      const response = await fetch('https://smsverify.vercel.app/api/create-checkout-session', {
+      const response = await fetch('https://smsverify-server.vercel.app/api/create-checkout-session', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
